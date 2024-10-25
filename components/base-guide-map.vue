f701f3673582531b68f49ef95a2f030a318c5aac
HoganK <ooss87968863@gmail.com>
2024-10-26 03:03:58 +0800

    [add] 新增導覽元件

---
+++ components/base-guide-map.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const path = route.path.startsWith('/') ? route.path.slice(1) : route.path
+const guideMap = ref([])
+guideMap.value.push(path)
+if (route.query.keyword) {
+  guideMap.value.push(route.query.keyword)
+}
+</script>
+
+<template>
+  <QBreadcrumbs class="q-pa-xl text-primary" active-color="dark">
+    <template #separator>
+      <QIcon name="fa-solid fa-angle-right" color="dark" />
+    </template>
+
+    <QBreadcrumbsEl icon="fa-solid fa-house" />
+    <QBreadcrumbsEl v-for="(item, index) in guideMap" :key="index" :label="item" />
+  </QBreadcrumbs>
+</template>
